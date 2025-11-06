8f93c1abe3bd252b8eb31f855cee6dd941e66974
Ruth-Ann Armstrong <rharmstr@endeavour2.hpc.usc.edu>
2025-11-06 02:07:38 -0800

    fix source file

--- model_output_annotation.py
+++ model_output_annotation.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Human Evaluation", layout="wide")
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/eval_pairs.csv")
+    return pd.read_csv("eval_pairs.csv")
 
 df = load_data()
 
